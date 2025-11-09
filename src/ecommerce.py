9092e4b369c64103e931d068bda10038d5fed3a4
Ben Rinius <benrinius@gmail.com>
2025-11-09 07:30:05 +0100

    Updated notebooks, modules and ecommerce.py, added .gitignore

--- src/ecommerce.py
+++ src/ecommerce.py
@@ -27,3 +27,10 @@ class EcommerceAnalyzer:
     def barStader(self, df):
         barChartStader = viz.barStader(df)
         return barChartStader
+    
+    
+    def total_revenue(self, df):
+        return metrics.total_revenue(df)
+    
+    def total_units(self, df):
+        return metrics.total_units(df)
